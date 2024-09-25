5064e60dfc5b703200fb9d7706b085f201dd2207
Nadine von Frankenberg <vonfrankenberg@pitt.edu>
2024-09-25 15:14:50 -0400

    Update CauldronEvent.java
    Fix typo 3->4 to actually have a 25% chance of an exploding cauldron

--- src/CauldronEvent.java
+++ src/CauldronEvent.java
@@ -11,7 +11,7 @@ import java.util.Random;
 public class CauldronEvent {
     public boolean triggerEvent() {
         Random random = new Random();
-        int event = random.nextInt(3);
+        int event = random.nextInt(4);
 
         if (event == 0) {
             System.out.println("Oh no! Your potion exploded and is useless!");
